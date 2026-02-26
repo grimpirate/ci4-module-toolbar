63d06a3119b007f9e693436928a24a1056d138b7
grimpirate <grimpirate_jrs@yahoo.com>
2026-02-26 17:40:45 -0500

    static method

--- modules/ComposerCollector/Collectors/ComposerCollector.php
+++ modules/ComposerCollector/Collectors/ComposerCollector.php
@@ -15,7 +15,7 @@ class ComposerCollector extends BaseCollector
 
 	protected array $updates = [];
 	protected int $count = 0;
-	protected Time $mtime = new Time();
+	protected Time $mtime;
 
 	public function __construct()
 	{
@@ -29,7 +29,7 @@ class ComposerCollector extends BaseCollector
 			}, ARRAY_FILTER_USE_KEY), json_decode(implode("", $output), true)['installed']);
 		});
 		$this->count = count($this->updates);
-		$this->mtime = $this->mtime->setTimestamp(cache()->getMetadata($config->cacheKey)['mtime']);
+		$this->mtime = Time::createFromTimestamp(cache()->getMetadata($config->cacheKey)['mtime']);
 	}
 
 	public function display(): string
