87b70a3983c9c455015a15aa4e0aa1cb1b4f6fd0
grimpirate <grimpirate_jrs@yahoo.com>
2026-02-27 22:50:28 -0500

    Typo fix

--- src/Module.php
+++ src/Module.php
@@ -7,7 +7,7 @@ use Michalsn\CodeIgniterModuleManager\BaseModule;
 class Module extends BaseModule
 {
 	protected string $name = 'Composer Collector';
-	protected string $description = 'A module for CodeIgniter's debug toolbar that displays information about outdated composer packages';
+	protected string $description = 'A module for CodeIgniter\'s debug toolbar that displays information about outdated composer packages';
 	protected string $version = '1.0.0';
 	protected string $author = 'Grim Pirate';
 	protected ?string $url = 'https://github.com/grimpirate/ci4-module-toolbar';
